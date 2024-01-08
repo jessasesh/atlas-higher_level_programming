def97f1e454efb5fa8a2143c178576c3bb0b65ef
jessasesh <jessica.dison@yahoo.com>
2024-01-08 20:02:27 +0000

    Revised variable name

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for numb in range(len(my_list)):
-        print("{:}".format(my_list[numb]))
+    for i in range(len(my_list)):
+        print("{:}".format(my_list[i]))
