1e52458251d5afa1c5f7971cea537e9ea67a9dd1
jessasesh <jessica.dison@yahoo.com>
2024-01-09 05:30:52 +0000

    Corrected syntax

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Prints a string in uppercase
 def uppercase(str):
-    uppercase_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
-    print(uppercase_char, end="")
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        print(uppercase_char, end="")
     print("")
