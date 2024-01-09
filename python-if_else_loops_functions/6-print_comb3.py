a562a0a5ccf3ca17a617532c00d52afa57104cd6
jessasesh <jessica.dison@yahoo.com>
2024-01-09 05:03:06 +0000

    Updated loop

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 #Prints all possible different combinations of two digits
 
-for tens in range(10):
-    for ones in range(tens, + 1, 10):
-        f = ("{:02d}".format(tens * 10 + ones)
-        print(f, end=", ")
+for z in range(10):
+    for x in range(10):
+        if z < x:
+            if z == 8 and x == 9:
+                print("{}{}".format(z, x))
+            else:
+                print("{}{}, ".format(z, x))
