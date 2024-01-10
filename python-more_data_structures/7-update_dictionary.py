7ebcc92c501da2da49a2cb47e560dcaef63776fb
jessasesh <jessica.dison@yahoo.com>
2024-01-10 04:49:16 +0000

    Fixed error

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def update_dictionary(my_dict, key, value):
-    my_dict[key] = value
-    return my_dict
+# Function that replaces or adds key/value in a dictionary
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
