2a0117f41d4125aab506705f96cec9b0e1d9c114
jessasesh <jessica.dison@yahoo.com>
2024-01-10 02:54:01 +0000

    Added comments

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
-# Prints a matrix of integers
-
+'''
+Function that takes a 2D matrix as an argument
+with a default value of an empty list containing 
+an empty list; prints a matrix of integers
+'''
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, element in enumerate(row):
-            print("{:d}".format(element), end="" if i == len(row) - 1 else " ")
+    
+    # Outer loop iterating each row
+    for c_row in range(len(matrix)):
+        
+        # Inner loop iterating through each element in current row
+        for c_element in range(len(matrix[c_row])):
+            
+            # If the current element is in last row
+            if c_element == len(matrix[c_row]) - 1:
+                
+                # Print the formatted element without a space
+                print("{:d}".format(matrix[c_row][j]), end='')
+            
+            # If current element is not in last row
+            else:
+                
+                # Prints formatted element with a space
+                print("{:d}".format(matrix[c_row][c_element]), end=' ')
+        # Prints newline character
+        print()
