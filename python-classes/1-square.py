ad6f653044e5cfbb23bd379b33d66a4594ba96bd
jessasesh <jessica.dison@yahoo.com>
2024-01-16 22:45:02 +0000

    Changed file perms

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a class named square"""
+
+class Square:
+
+    def __init__(self, size):
+        """Initializes new square
+
+        Args:
+            size (int): the size of new square
+        """
+        self.__size = size
