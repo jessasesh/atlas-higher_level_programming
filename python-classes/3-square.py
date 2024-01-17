d00beb04572a9bbcd6cc8f887118ddb66de9ff84
jessasesh <jessica.dison@yahoo.com>
2024-01-17 02:05:48 +0000

    Added function

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Defines a class named square"""
+
+
+class Square:
+    """Names and represents square"""
+
+    def __init__(self, size=0):
+        """Initializes new square
+
+        Args:
+            size (int): the size of new square
+            (private instance attribute)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Determines the area of the square
+
+        Returns:
+            The area of the square
+        """
+       area = (self.__size * self.__size)
+       return area
