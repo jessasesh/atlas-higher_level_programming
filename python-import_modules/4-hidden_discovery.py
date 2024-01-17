a44453af4b0352a4e512efc6e7f348f78c1f3cb6
jessasesh <jessica.dison@yahoo.com>
2024-01-17 16:58:06 +0000

    Added docstring and changed file perms

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Program that prints all the names defined by
+the compiled module hidden_4.pyc
+"""
+import hidden_4
+
+if __name__ == "__main__":
+
+    # Loop that iterates over member within 
+    # hidden_4 module
+    for member_name in dir(hidden_4):
+        # Checks if member is not hidden
+        if member_name[0] != "_":
+            # Prints the name of member
+            print(member_name)
