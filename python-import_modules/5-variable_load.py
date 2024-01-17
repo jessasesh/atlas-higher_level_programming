8565dc7b81109ad0cdbbb87d763e0c26e464fd34
jessasesh <jessica.dison@yahoo.com>
2024-01-17 17:38:47 +0000

    Added provided file & import code

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Program that imports the variable a from the
+   file variable_load_5.py and prints its value
+"""
+from variable_load_5 import a
+
+if __name__ == "__main__":
+    # Prints the value of 'a'
+    print(a)
