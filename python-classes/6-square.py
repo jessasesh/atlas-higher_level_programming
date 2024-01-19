ff8c286784eb93e7074fd89bde6b1359eedb2166
jessasesh <jessica.dison@yahoo.com>
2024-01-19 02:21:25 +0000

    Corrected pycode

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,19 +4,20 @@ Program that defines a square
 
 """
 class Square:
+
     """
     Defines class named Square
     """
     # Initialize data
     def __init__(self, size=0, position=(0, 0)):
-        
+
         # Checks size parameter
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         # Checks position parameter
         if not isinstance(position, tuple) or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -29,35 +30,35 @@ class Square:
 
         if position[1] < 0 or position[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        
+
         # Initialize instance variables
         self.__size = size
         self.__position = position
-    
+
     # Calculate area of square
     def area(self):
         area = self.__size * self.__size
         return area
-    
+
     # Retrieves square size
     @property
     def size(self):
         return self.__size
-    
+
     # Sets size of square
     @size.setter
     def size(self, value):
-        
+
         # Check and set size
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
         if value < 0:
             raise ValueError("size must be >= 0")
-        
+
         # Resets value to new size
         self.__size = value
-    
+
     # Retrieves square postition
     @property
     def position(self):
@@ -76,7 +77,7 @@ class Square:
 
         if value[1] < 0 or value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        
+
         # Resets position
         self.__position = value
 
@@ -85,12 +86,12 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        
+
         # Prints empty lines for vertical offset
         if self.__position[1] != 0:
             for x in range(self.__position[1]):
                 print("")
-        
+
         # Prints spaces and symbols to create square
         for col in range(self.__size):
             if self.__position[0] > 0:
