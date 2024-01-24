7e369798ad026f305c5cb1fef16639624d9856b8
jessasesh <jessica.dison@yahoo.com>
2024-01-24 03:34:22 +0000

    Added exception message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines an empty class named BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Represent base geometry.
+    """
+    def area(self):
+        """
+        Required exception message
+        """
+        raise Exception("area() is not implemented")
