112beaa89de5fa2bad881fa0b549b9cec2595ed4
jessasesh <jessica.dison@yahoo.com>
2024-01-24 00:33:05 +0000

    Corrected indentation error

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-    """
-    This is a function that returns the
-    list of available attributes and
-    methods of an object.
-    """
+"""
+This is a function that returns the
+list of available attributes and
+methods of an object.
+"""
 
 
 def lookup(obj):
