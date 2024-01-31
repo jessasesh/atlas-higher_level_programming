5373ef3c22831590cbd81433963e96ac7b363cc4
jessasesh <jessica.dison@yahoo.com>
2024-01-31 04:05:37 +0000

    Added proto

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This function reads a text file and
+prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Opens, reads, and prints text file 
+    to stdout.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
