8b71daad8e78ead5cae95341981a28cc84f01327
jessasesh <jessica.dison@yahoo.com>
2024-01-31 02:40:42 +0000

    Added id tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
 """unittests for base.py"""
+
+import unitest
+from models.base import Base
+
+class TestBase_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of Base class"""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_auto_id(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual([b1.id, b2.id, b3.id], [1, 2, 3])
+
+
