ede4040a7d2d7d83c591a4cf476e1aa4410fb0c3
jessasesh <jessica.dison@yahoo.com>
2024-01-31 04:35:10 +0000

    Added docs

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This function that returns the JSON
+representation of an object.
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of a string object.
+    """
+    return json.dumps(my_obj)
