b7ea9c88025b614ab2c5f18685802f62399390d0
jessasesh <jessica.dison@yahoo.com>
2024-02-23 04:08:39 +0000

    See if this checks

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,6 +10,7 @@ MySQL injections!
 import MySQLdb as db
 from sys import argv
 
+
 if __name__ == "__main__":
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
