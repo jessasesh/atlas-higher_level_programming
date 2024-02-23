45aa13e28edc9ad93e0d659e332a90fe4e64b3c4
jessasesh <jessica.dison@yahoo.com>
2024-02-23 02:54:12 +0000

    Corrected syntax

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
-Lists all states from specified database
-"""
-import sys
+"""  Lists all states from specified database """
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-      host="localhost",
-      user=sys.argv[1],
-      passwd=sys.argv[2],
-      database=sys.argv[3],
-      port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    results = cur.fetchall()
-    for result in results:
-        print(result)
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
