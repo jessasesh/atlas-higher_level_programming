4a56360c9f24fb7e13fef3c50205b3cb2549aa6e
jessasesh <jessica.dison@yahoo.com>
2024-02-23 04:44:16 +0000

    Corrected PEP errors

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains a definition of a state and instance
+Contains State class and Base, an instance
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,7 +10,7 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-        """
+    """
     Class with id and name attributes
     """
     __tablename__ = 'states'
