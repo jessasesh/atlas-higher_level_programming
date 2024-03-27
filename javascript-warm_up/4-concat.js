fb068436674ead43a78bd322fc0057a58334ba30
jessasesh <jessica.dison@yahoo.com>
2024-03-27 03:46:11 +0000

    Removed error msg for checker;/

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,7 +3,4 @@
 if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
   // Print the two arguments in the specified format
   console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  // Print an error message if two arguments are not provided
-  console.log('Error: Please provide exactly two arguments.');
 }
