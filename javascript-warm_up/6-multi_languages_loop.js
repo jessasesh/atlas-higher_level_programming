80a76d913c51f54bcb8385a9563c4985a76ef225
jessasesh <jessica.dison@yahoo.com>
2024-03-27 04:10:08 +0000

    Changed file perms

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Define an array of strings
+const sayWhat = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Initialize a loop counter
+let i = 0;
+
+// Loop through the array and print each string
+for (i = 0; i < sayWhat.length; i++) {
+    console.log(sayWhat[i]);
+}
