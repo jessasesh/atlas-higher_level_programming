38b667899145d45cab6081117f0bc1b263973fe7
jessasesh <jessica.dison@yahoo.com>
2024-03-27 03:52:30 +0000

    Different approach; pipe to handle undefined

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// Check if two arguments are passed to the script
-if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-  // Print the two arguments in the specified format
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+// Check if two arguments are passed, concats, and prints
+console.log(
+	(process.argv[2] || 'undefined') + ' is ' + (process.argv[3] || 'undefined')
+);
