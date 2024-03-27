47b327ff7b330a4f5ddde126af6b4522a3de9929
jessasesh <jessica.dison@yahoo.com>
2024-03-27 04:34:55 +0000

    Loop wasn't looping

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 // Parse the first argument to an integer
-const input = Number(process.argv[2]);
-
-// Check if the parsed integer is not a number or if it's not provided
-if (isNaN(input)) {
-  // Handle if it's not a number or not provided
-  console.log("Missing number of occurrences");
-} else {
-  // Handle for valid input
-  for (let i = 0; i < input; input++) {
-    console.log("C is fun");
-  }
+const argv2 = process.argv[2];
+// Handle bad input
+if (!process.argv[2]) {
+  console.log('Missing number of occurrences');
+}
+// Allows iteration of print statement to be specified
+for (let x = 0; x < argv2; x++) {
+  console.log('C is fun');
 }
