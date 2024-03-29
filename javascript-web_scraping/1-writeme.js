a499cdcfba8ba6b8368d11128089017d37a7fd1d
jessasesh <jessica.dison@yahoo.com>
2024-03-29 22:46:32 +0000

    Added error handling

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/* Import File System module */
+const fs = require('fs');
+
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+
+// Get the string to write from command line arguments
+const toWrite = process.argv[3];
+
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, toWrite, 'utf8', function (error) {
+  // Check if an error occurred during writing
+  if (error) {
+    console.error(error); // Print the error object if an error occurred
+  }
+});
