b97a97e72e8a170395044108615c1abcfeea12b1
jessasesh <jessica.dison@yahoo.com>
2024-04-02 02:28:03 +0000

    Added comments

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
+// Import modules
 const request = require('request');
 const fs = require('fs');
 
+// Extract URL and file path from
+// command line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
+// Make a GET request to the specified URL
 request.get(url, function (error, response, body) {
+  // Write the response to specified file
   fs.writeFile(filePath, body, 'utf8', function (error) {
     if (error) {
       console.error(error);
