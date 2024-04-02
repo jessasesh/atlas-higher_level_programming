f3614395107c935dd806772325ff66f0769e9678
jessasesh <jessica.dison@yahoo.com>
2024-04-02 04:22:33 +0000

    Error handling

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -3,12 +3,17 @@ $(document).ready(function () {
   // Select the DIV#red_header element
   const redHeader = $('div#red_header');
 
-  // Attach a click event listener to the redHeader element
-  redHeader.on('click', function () {
-    // Select the <header> element
-    const header = $('header');
+  // Check if the redHeader element exists
+  if (!redHeader.length) {
+    console.error('Error: The "red_header" element does not exist.');
+  } else {
+    // Attach a click event listener to the redHeader element
+    redHeader.on('click', function () {
+      // Select the <header> element
+      const header = $('header');
 
-    // Add the class 'red' to the <header> element
-    header.addClass('red');
-  });
+      // Add the class 'red' to the <header> element
+      header.addClass('red');
+    });
+  }
 });
