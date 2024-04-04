a256e4aff5fb3455830fd59bcfa2e866754bf41a
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-04-03 21:06:32 -0500

    Added comment lines for understanding

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,13 +1,25 @@
+// Wait for the document to be fully loaded
 $(function() {
+  // URL for the API endpoint
   const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 
+  // Perform a GET request to the API endpoint
   $.get(url, function(data) {
+    // Extract the results array from the response data
     const results = data.results;
+    
+    // Select the UL element with ID "list_movies"
     const listMovies = $('UL#list_movies');
 
+    // Loop through each movie in the results array
     for (let i = 0; i < results.length; i++) {
+      // Extract the title of the current movie
       const movieTitle = results[i].title;
+      
+      // Create a new list item element with the movie title as its text content
       const listItem = $('<li>').text(movieTitle);
+      
+      // Append the new list item to the UL element
       listMovies.append(listItem);
     }
   });
