edb9dd10bc847515a6a1688f9a108263d42ebfb7
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-21 23:49:46 -0500

    Determines whether the provided object is an instance of the specified class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module containing the 'same_class' function, which checks whether
+the provided object is an exact instance of a specified class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that determines whether the provided object is an instance of the specified class.
+
+    Args:
+        obj: The object to evaluate.
+        a_class: The class type to check against.
+
+    Returns:
+    True if obj is an exact instance of a_class, otherwise False.
+    """
+    return type(obj) is a_class
