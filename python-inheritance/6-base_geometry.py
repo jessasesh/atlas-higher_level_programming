c5899ca6093a137f271832ba9f835bac630462a8
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-21 23:59:33 -0500

    Basic and unfinished method for calculating area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Definition of the 'BaseGeometry' class.
+    """
+    def area(self):
+        """
+        Basic and unfinished method for calculating area.
+        """
+        raise Exception("area() is not implemented")
+    
