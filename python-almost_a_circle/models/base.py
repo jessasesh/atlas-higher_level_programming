61c80b1645ba729518f11f5995f18e8dfaa34c11
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-29 10:55:52 -0500

    Added json return method to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Write class to manage id attribute in all
 future classes and to avoid duplicating
 the same code.
 """
+import json
 
 
 class Base:
@@ -22,3 +23,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Reutrns json string representation.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
