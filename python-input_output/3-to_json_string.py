17674488a5062708c168fb3431d62607ebe5d68f
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-28 22:38:23 -0500

    Added json dump to method

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -3,6 +3,11 @@
 Function that returns the JSON representation
 of an object (string).
 """
+import json
 
 
 def to_json_string(my_obj):
+    """
+    Returns JSON representation of a string object.
+    """
+    return json.dumps(my_obj)
