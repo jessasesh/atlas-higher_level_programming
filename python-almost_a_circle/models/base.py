899132babe20e5ab6554ce2d4f4df3dfe3d4852f
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-29 11:13:25 -0500

    Fix for syntax

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,7 +29,7 @@ class Base:
         """
         Returns json string representation.
         """
-        if list_dictionaries is None or len(list_dictionaries) == "":
-            return []
+        if list_dictionaries is None:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
