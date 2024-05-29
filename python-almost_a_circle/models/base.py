a20b526d938a55bfef69a29a1d857544e21f2146
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-29 11:10:36 -0500

    Fix for syntax errors

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,11 +25,11 @@ class Base:
             self.id = Base.__nb_objects
     
     @staticmethod
-    def to_json_string(self, list_dictionaries):
+    def to_json_string(list_dictionaries):
         """
         Returns json string representation.
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+        if list_dictionaries is None or len(list_dictionaries) == "":
+            return []
         else:
-            return json.dumps(list_dictionaries)
+            return json.loads(list_dictionaries)
