8d30046ac7a1ed9a19de8629b403ba9113b2c090
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-28 21:01:03 -0500

    First class with specified attribute & constructor

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write class to manage id attribute in all
+future classes and to avoid duplicating
+the same code.
+"""
+
+
+class Base:
+    """
+    Base class.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+         Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
