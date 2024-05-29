271cae473e669d442540a0f9d8130ef0d87fd885
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-28 22:42:00 -0500

    Function that returns an object represented by a JSON string.

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,6 +3,11 @@
 Function that returns an object (Python data structure)
 represented by a JSON string.
 """
+import json
 
 
 def from_json_string(my_str):
+    """
+    Returns object represented by JSON string.
+    """
+    return json.loads(my_str)
