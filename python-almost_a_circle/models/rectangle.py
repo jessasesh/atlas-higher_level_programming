d6cc43fbbebc0995c9e734eb2e81953a92091430
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-28 21:49:11 -0500

    Updated class with area value method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,9 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Returns area of Rectangle.
+        """
+        return self.height * self.width
