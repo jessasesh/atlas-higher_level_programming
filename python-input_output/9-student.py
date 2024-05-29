746358628346e38811f0e57f054086c9318e8aa2
jessasesh <126801159+jessasesh@users.noreply.github.com>
2024-05-29 12:02:03 -0500

    Created class with attributes; implemented method to retrieve student info

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,11 +4,20 @@ Write a class with instance retrieval.
 """
 
 
-Student
-first_name
-last_name
-age
+class Student:
+    """
+    Representation of a student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of class.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def __init__(self, first_name, last_name, age):
-
-def to_json(self):
+    def to_json(self):
+        """
+        Retrieves dictionary of student and attributes.
+        """
+        return self.__dict__
